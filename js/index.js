04a064af7ba50bed5f5cd1a5ff56daafdf5fa8a3
oOzhuoxiaOo <1851428933@qq.com>
2024-10-17 23:17:24 +0800

    fix(#1): 文件类型不是图片,也对img赋值了

--- js/index.js
+++ js/index.js
@@ -6,8 +6,10 @@ let deleteBtnDom =  document.querySelector('.delete-btn') // 删除按钮
 let imgDom = document.querySelector('.o-img') // 上传图片展示区域
 let submitBtnDom = document.querySelector('.submit-btn') // 提交按钮
 
+
 // 上传的文件Blob对象
 let targetFile = null
+let defaultImgUrl = imgDom.src
 
 // 校验文件 【支持格式：doc、docx、pdf、png，最大10M】
 function checkFile(file) {
@@ -20,6 +22,16 @@ function checkFile(file) {
     }
     if (size > 10 * 1024 * 1024) {
         alert('文件大小不能超过10M')
+        return false
+    }
+    return true
+}
+
+// 校验文件是否是图片png
+function checkImage(file) {
+    let type = file.type
+    let typeReg = /png/
+    if (!typeReg.test(type)) {
         return false
     }
     return true
@@ -45,10 +57,10 @@ fileInputDom.addEventListener('change', function() {
     let reader = new FileReader()
     // 读取文件
     reader.readAsDataURL(targetFile)
-    // 读取完毕后，将图片显示到页面上
+    // 读取完毕后，如果是图片，将图片显示到页面上，如果不是则显示默认图片
     reader.onload = function() {
         toggleVisibility()
-        imgDom.src = reader.result
+        imgDom.src = checkImage(targetFile) ?  reader.result : defaultImgUrl
     }
 })
 
